0459ff91a6bd22789406d5ad001348067f585838
Yusuf Abdullahi Temidayo <yusufabdullahi2910@gmail.com>
2024-09-27 05:23:08 +0100

    updated

--- src/components/SaveTimeSection.tsx
+++ src/components/SaveTimeSection.tsx
@@ -3,18 +3,20 @@ import DashboardImg from "../../assets/img/collaborate.png";
 
 const SaveTimeSection: React.FC = () => {
   return (
-    <section className="bg-white py-20 px-6 lg:px-[6.95%]  space-y-[14px]">
-      <div className="w-[42%] justify-center items-center text-center">
-        <h1 className="text-[41px] text-[#24223e] font-black leading-tight">
+    <section className="bg-white py-20 px-6 lg:px-0 space-y-[14px]">
+      {/* Main content */}
+      <div className="w-full flex flex-col justify-center items-center text-center lg:px-[32%] lg:pb-20">
+        <h1 className="text-[48px] text-[#24223e] font-black leading-tight">
           Save time and get more done
         </h1>
-        <p className="text-lg text-[#292d34] font-semibold">
+        <p className="text-xl text-[#292d34] font-semibold">
           Supercharge productivity. Streamline work by doing it, and seeing it,
           in one place.
         </p>
       </div>
-      <div className="flex flex-row">
-        <div className="flex flex-col space-y-4 items-start">
+
+      <div className="flex flex-row justify-center items-center space-x-6 lg:pl-[10%]">
+        <div className="flex flex-col space-y-4 items-start max-w-xl">
           <h1 className="text-4xl font-black bg-gradient-to-r from-blue-600 via-pink-500 to-purple-600 bg-clip-text text-transparent">
             Improve collaboration
           </h1>
@@ -22,11 +24,12 @@ const SaveTimeSection: React.FC = () => {
             <b>
               Get your teams working together more closely, even if they’re far
               apart.
-            </b>{" "}
+            </b>
             Centralize project-related communications in one place, brainstorm
             ideas with Whiteboards, and draft plans together with collaborative
             Docs.
           </p>
+
           <div className="space-x-2">
             <span className="text-lg font-normal text-[#292d34] border-[1px] rounded-lg shadow-md py-2 border-gray-300 px-4 hover:bg-slate-100">
               Comments
@@ -41,16 +44,17 @@ const SaveTimeSection: React.FC = () => {
               Clips
             </span>
           </div>
-          <h1 className="text-4xl font-black bg-gradient-to-r from-blue-600 via-pink-500 to-purple-600 bg-clip-text text-transparent py-4">
+
+          <a href="/" className="text-4xl font-black text-gray-400 hover:bg-gradient-to-r from-blue-600 via-pink-500 to-purple-600 hover:bg-clip-text hover:text-transparent py-4">
             Enhance visibility
-          </h1>
-          <h1 className="text-4xl font-black bg-gradient-to-r from-blue-600 via-pink-500 to-purple-600 bg-clip-text text-transparent py-4">
+          </a>
+          <a href="/" className="text-4xl font-black text-gray-400 hover:bg-gradient-to-r from-blue-600 via-pink-500 to-purple-600 hover:bg-clip-text hover:text-transparent py-4">
             Automate work
-          </h1>
+          </a>
         </div>
-        <div
-          className="flex">
-          <img src={DashboardImg} alt="DashboardImg" className="top-0 left-20" />
+
+        <div className="flex justify-center items-center bg-gradient-to-r from-blue-600 to-purple-600 mr-[-20px]">
+          <img src={DashboardImg} alt="DashboardImg" className="w-auto h-auto" />
         </div>
       </div>
     </section>
